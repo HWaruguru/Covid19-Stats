dd4a3e625ca6bf92d4e53160fd3187c8fde69edf
otienonick <nick.odongi@student.moringaschool.com>
2021-09-28 07:19:14 +0300

    added initial files

---
+++ manage.py
@@ -0,0 +1,25 @@
+from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+from app.models import User,Todo
+
+
+# creating app instance
+app = create_app('development')
+
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Todo = Todo) 
+
+if __name__ == '__main__':
+    manager.run()
+
+#   whenever you make a change to your python code it will automatically rerun the flask web server
