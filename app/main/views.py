835af94e3f04d0c1daf995820efa47c9c419a41c
otienonick <nick.odongi@student.moringaschool.com>
2021-09-28 12:36:11 +0300

    created adding covid statistics endpoints

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from . import main
 from flask import request,jsonify,make_response
 from werkzeug.security import generate_password_hash,check_password_hash
-from ..models import User
+from ..models import User,Covid
 import uuid
 from .. import db
 
@@ -42,6 +42,7 @@ def create_user():
     db.session.commit()
     return jsonify({'message': 'new user created!'}) 
 
+
 @main.route('/user/<public_id>',methods = ['DELETE'])
 def delete_user(public_id):
     user = User.query.filter_by(public_id = public_id).first()
@@ -50,6 +51,7 @@ def delete_user(public_id):
     db.session.delete(user)
     db.session.commit()    
     return jsonify({'message' : 'User has been deleted'})    
+
 
 @main.route('/login')     
 def login():
@@ -65,7 +67,67 @@ def login():
         return jsonify ({'message' : 'Login successful!'})
     return jsonify({'message' : 'wrong username or password!'})    
 
+@main.route('/user/<public_id>/post',methods = ['POST'])
+def create_post(public_id):
+    user = User.query.filter_by(public_id = public_id).first()
+    if user:
+        data = request.get_json()
+        new_post = Covid(country = data['country'],cases = data['cases'],date_created = data['date_created'],user_id = data['user_id'])
+        db.session.add(new_post)
+        db.session.commit()
+        return jsonify({'message': 'new covid post created!'}) 
+    return jsonify({'message' : 'No user found!'})
+
+@main.route('/user/post', methods = ['GET'])
+def get_all_posts():
+    posts = Covid.query.all()
+    output = []
+    for post in posts:
+        post_data = {}
+        post_data['country'] = post.country
+        post_data['cases'] = post.cases
+        post_data['date_created'] = post.date_created
+        # post_data['user_id'] = post.user_id
+
+        output.append(post_data)
+
+    return jsonify({'posts' : output})   
+
+@main.route('/user/<id>/post', methods = ['GET'])
+def get_one_userpost(id):
+    post = Covid.query.filter_by(id = id).first()
+    if not post:
+        return jsonify({'message' : 'No post found!'})
+    post_data = {}
+    post_data['country'] = post.country
+    post_data['cases'] = post.cases
+    post_data['date_created'] = post.date_created
+
+    return jsonify({'post' : post_data})     
     
+
+@main.route('/user/<user_id>',methods = ['DELETE'])
+def delete_post(user_id):
+    post = Covid.query.filter_by(user_id = user_id).first()
+    if not post:
+        return jsonify({'message' : 'No user found!'})
+    db.session.delete(post)
+    db.session.commit()    
+    return jsonify({'message' : 'Post has been deleted'})    
+
+
+@main.route('/user/<public_id>/post',methods = ['PUT'])
+def update_post(public_id):
+    user = User.query.filter_by(public_id = public_id).first()
+    if user:
+        data = request.get_json()
+        new_post = Covid(country = data['country'],cases = data['cases'],date_created = data['date_created'],user_id = data['user_id'])
+        db.session.add(new_post)
+        db.session.commit()
+        return jsonify({'message': 'covid post successfully updated!'}) 
+    return jsonify({'message' : 'No user found!'})
+
+     
     
            
 
